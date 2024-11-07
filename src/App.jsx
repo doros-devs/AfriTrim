49d12fc26997edd19e1ec1295fa37a9fc6e79aa3
TheRealTeza <78547659+doros-devs@users.noreply.github.com>
2024-11-07 10:33:55 +0300

    Update App.jsx
    updated the issue with forgotpassword components

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
-import ForgotPassword from "./components/ForgotPassword";
+import ForgotPassword from "./components/Forgotpassword";
 import LandingPage from "./components/LandingPage";
 
 const App = () => {
@@ -12,7 +12,7 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/forgot-password" element={<Forgotpassword />} />
       </Routes>
     </Router>
   );
